4ee9624af5e3bd0819d5e7ac7802511812b7edd1
mramiroz <manuel.ramiro.zabaleta@gmail.com>
2022-09-19 09:09:10 +0200

    ft_memcpy

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrarmiro- <mramiro-@student.42madrid.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/19 08:53:11 by mrarmiro-         #+#    #+#             */
+/*   Updated: 2022/09/19 08:53:11 by mrarmiro-        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void *ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+
+	if(!dest && !src)
+		return (0);
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+int main(){
+	char d[7];
+	char s[] = "asdfgh"
+	ft_memcpy(d, s, )
+}
