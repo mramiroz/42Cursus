a18f6b5993ae701a1e42694a657559656816b671
mramiroz <manuel.ramiro.zabaleta@gmail.com>
2022-09-26 10:11:53 +0200

    strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrarmiro- <mramiro-@student.42madrid.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 09:32:24 by mrarmiro-         #+#    #+#             */
+/*   Updated: 2022/09/26 09:32:24 by mrarmiro-        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+#include <stdlib.h>
+
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    int i;
+    int len1;
+    int len2;
+    char *s;
+
+    i = 0;
+    len1 = ft_strlen(s1);
+    len2 = ft_strlen(s2);
+    s = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+    if (s == NULL)
+        return (0);
+    i = -1;
+    while (s1[++i])
+    {
+        s[i] = s1[i];
+    };
+    i = -1;
+    while (s2[++i])
+    {
+        s[len1] = s2[i];
+        len1++;
+    }
+    s[len1] = '\0';
+    return (s);
+}
