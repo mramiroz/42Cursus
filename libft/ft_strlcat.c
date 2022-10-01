66ba5882c55ea2394e60d207af1de014087869f3
Manuel Ramiro Zabaleta <mramiro-@c3r3s3.42madrid.com>
2022-10-01 11:50:30 +0200

    strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrarmiro- <mramiro-@student.42madrid.co    +#+  +:+       +#+        */
+/*   By: mramiro- <mramiro-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 12:49:41 by mramiro-          #+#    #+#             */
-/*   Updated: 2022/09/29 19:47:29 by mrarmiro-        ###   ########.fr       */
+/*   Updated: 2022/10/01 11:49:47 by mramiro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	size_t len_src;
-	size_t len_dest;
-	size_t i;
-	size_t len;
+	size_t	len_src;
+	size_t	len_dest;
+	size_t	i;
+	size_t	len;
+	size_t	n;
 
+	n = 0;
 	len_dest = ft_strlen(dest);
 	len_src = ft_strlen(src);
 	len = len_dest + len_src;
 	i = 0;
 	if (dstsize <= len_dest)
-		return(len_src + dstsize);
-	while (*dest)
-		dest++;
-	while ((i < dstsize - 1) && src[i])
+		return (len_src + dstsize);
+	while (dest[n])
+		n++;
+	while ((n < dstsize - 1) && src[i])
 	{
-		dest[i] = src[i];
+		dest[n] = src[i];
 		i++;
+		n++;
 	}
-	dest[i] = '\0';
+	dest[n] = '\0';
 	return (len);
 }
