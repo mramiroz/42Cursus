e62eddbfb5bcc3157039077940b460e84f3f7c12
Manuel Ramiro Zabaleta <mramiro-@c3r3s3.42madrid.com>
2022-10-01 11:23:11 +0200

    memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -3,25 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrarmiro- <mramiro-@student.42madrid.co    +#+  +:+       +#+        */
+/*   By: mramiro- <mramiro-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:14:26 by mramiro-          #+#    #+#             */
-/*   Updated: 2022/09/30 08:42:17 by mrarmiro-        ###   ########.fr       */
+/*   Updated: 2022/10/01 11:22:32 by mramiro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void	*ft_memmove(void *str1, const void *str2, size_t n)
 {
-	size_t	i;
-	char	*sstr1;
-	char	*sstr2;
+	size_t			i;
+	unsigned char	*sstr1;
+	unsigned char	*sstr2;
 
 	i = 0;
-	sstr1 = (char *)str1;
-	sstr2 = (char *)str2;
+	sstr1 = (unsigned char *)str1;
+	sstr2 = (unsigned char *)str2;
 	while (i < n && (sstr1[i] != '\0' || sstr2[i] != '\0'))
 	{
 		sstr1[i] = sstr2[i];
